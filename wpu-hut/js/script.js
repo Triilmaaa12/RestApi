b3912b071be32f8b505dbc445209ed09db984a03
Triilmaaa12 <triilmaramadani23@gmail.com>
2025-05-28 23:32:47 +0700

    menyelesaikan video 5

--- wpu-hut/js/script.js
+++ wpu-hut/js/script.js
@@ -1,13 +1,15 @@
-$.getJSON('data/pizza.json', function (data) {
+
+    $.getJSON('data/pizza.json', function (data) {
     let menu = data.menu;
     $.each(menu, function(i, data) {
             $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/menu/'+ data.gambar +'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5 class="card-title">Rp. '+ data.harga +'</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
         });
 });
 
+
 $('.nav-link').on('click', function (){
     $('.nav-link').removeClass('active');
-    $(this).addClass('aktive');
+    $(this).addClass('active');
 
     let kategori = $(this).html();
     console.log(kategori);
@@ -18,7 +20,7 @@ $('.nav-link').on('click', function (){
         return;
     }
      function tampilkanSemuaMenu(){
-        
+
      }
     $.getJSON('data/pizza.json', function (data) {
         let menu = data.menu;
